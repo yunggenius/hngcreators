a5af2e9a8888210d334360f72c8e38ab28675095
yunggenius <yunggenius4@gmail.com>
2019-09-18 19:41:05 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title>login page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" type="text/css" href="css/main.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great&display=swap" rel="stylesheet">
+</head>
+<body>
+<!-- <h1 class="center">The Team Of The Creators</h1> -->
+  
 
-require('../vendor/autoload.php');
 
-$app = new Silex\Application();
-$app['debug'] = true;
+<div class="row">
+<div class= "card column to-left">
+	<h2 class="center">Welcome back</h2>	
+	<form>
+		 <label>
+                    <input type="text" name="username" placeholder="Username or email" >
+                </label>
+            <br><br><br>
+            <label>
+                <input type="password" name="password" placeholder="Password" >
+            </label>
+            <br><br><br>
+           
+            <input type="submit" class="btn" value="Login"><br><br>
+             <div class="afterform">
+            	<input type="checkbox"><a href="#" class="anchor">Remember me</a> <a class="anchor" href="#">Forgot password?</a>
+            </div><br>
+	</form>
+	<br><br>
+	<!--<hr class="x-axis"><span class="dot"></span></hr>-->
+<br>
+<div class="socio">
+<a href="#" class="fa fa-facebook"></a>
+<a  href="#" class="fa fa-slack" ></a>
+<a href="#" class="fa fa-twitter"></a>
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+<p>Don't have an account?<a href="#" class="anchor1">Sign up here</a></p>
+</div>
+</div>
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
 
-// Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+	<img class="img" src="img/major2.jpg">
 
-$app->run();
+</div>
+<div>
+	<img class="bottom-img" src="img/crid_desktopnew.png"></img>
+</div>
+
+</body>
+
+
+</html>
